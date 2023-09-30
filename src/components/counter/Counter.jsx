1a212f1145d3a633826e186e54e30f147be9db1d
reisel <reiselvalle@gmail.com>
2023-09-30 09:37:20 -0400

    split component wellcome

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 export const Counter = () =>{
     const [count, setCount] = useState(0)
     return (
-        <>
+        <>  
+            <h1>Counter component </h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
